0381d527d0d6e287b043c6d272f605a19845794c
Marton Aron Gyiran <gyiranmarton@gmail.com>
2025-01-02 19:50:32 +0100

    feat: base commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# daily-quest
+# DailyQuest
+
+Good to know:
+
+- you have to build before serve
+  - if it thorws an error, then open powershell in admin mode and run: `Set-ExecutionPolicy RemoteSigned`
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Grid2 from '@mui/material/Grid2';
+
+const Home = () => {
+	return (
+		<Grid2
+			direction='column'
+			container
+		>
+			<Grid2>
+				<p>test</p>
+			</Grid2>
+		</Grid2>
+	);
+};
+
+export default Home;
